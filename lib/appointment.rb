94fcf4c73c866b5dd70c539cafaab88284784a86
Kelin Field <kfield111@hotmail.com>
2020-08-26 03:11:15 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+  attr_accessor :date, :patient, :doctor
+
+  def initialize (date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+
+  def patient
+    patient
+  end
+
+  def doctor
+    doctor
+  end
+
+end
