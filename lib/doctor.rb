737981180fd18d0a4fad0182d9ab2be1341ad0cb
Kelin Field <kfield111@hotmail.com>
2020-08-26 03:08:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,5 +8,16 @@ class Doctor
       @@all << self
     end
 
+    def appointments
+      appointment.all.select {|temp| temp.doctor == self}
+    end
 
+    def new_appointment (date, patient)
+      Appointment.new(date, self, patient)
+    end
+
+    def patients
+      appointments.all.collect {|temp| temp.doctor == self}
+    end
+    
   end
