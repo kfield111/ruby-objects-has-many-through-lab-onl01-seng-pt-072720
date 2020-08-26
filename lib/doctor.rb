96271a4e7dbc1a94bae2973ba666c4a92e0c914b
Kelin Field <kfield111@hotmail.com>
2020-08-26 03:14:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor
-    attr_reader :name
+    attr_accessor :name
 
 @@all = []
 
@@ -19,5 +19,5 @@ class Doctor
     def patients
       appointments.all.collect {|temp| temp.doctor == self}
     end
-    
+
   end
