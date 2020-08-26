b74f71d05d666779e5b16a43300f31cc367e199d
Kelin Field <kfield111@hotmail.com>
2020-08-26 02:33:52 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,14 @@
 class Genre
   attr_reader :name
+  attr_accessor :artist, :song
+
+  @@all = []
 
   def initialize
     @name = name
+    @artist = artist
+    @song = song
+    @@all << self
   end
 
 
